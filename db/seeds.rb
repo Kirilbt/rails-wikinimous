d143b721ce5d5588a683657a3465e2c51aa8bbb8
Kiril Tucker <kiril.tucker@gmail.com>
2022-03-15 15:38:02 +0100

    Show done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times {
+  article = Article.new(
+    title: Faker::Science.science,
+    content: Faker::Lorem.paragraph(sentence_count: 5)
+  )
+  article.save!
+}
+puts 'Articles added'
